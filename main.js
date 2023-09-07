0834408737c2e81ce8ef4b815048508cc28ec0eb
Joel Josy <joeljosy79@gmail.com>
2023-09-07 23:12:44 +0530

    get no of tiles as input

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let grid = document.querySelector('.grid');
 
+// Get no of tiles input
+const input = prompt("Enter number of tiles");
+grid.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
 
 // Create tiles
-for (let i = 0; i < (16**2); i++) {
+
+for (let i = 0; i < (input**2); i++) {
     grid.appendChild(document.createElement('div'));
 }
 
@@ -11,6 +15,6 @@ for (let i = 0; i < (16**2); i++) {
 let divs = document.querySelectorAll('.grid > div')
 divs.forEach((div) => {
     div.addEventListener('mouseover', () => {
-        div.classList.add('tileHover')
-    })
+        div.classList.add('tileHover');
+    });
 });
