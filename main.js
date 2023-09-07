6cc44a2e545840e515e603b42016e8d62d80532f
Joel Josy <joeljosy79@gmail.com>
2023-09-08 00:25:19 +0530

    Add range slider

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 let grid = document.querySelector('.grid');
 
 // Get no of tiles input
-const input = prompt("Enter number of tiles");
-grid.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+let slider = document.getElementById('rangeSlider');
+let rangeOut = document.getElementById('rangeOutput');
+let sliderValue = slider.value;
+
+
+slider.oninput =  () => {
+    sliderValue = slider.value;
+    rangeOut.innerHTML = sliderValue;
+};
+
 
 // Create tiles
-
-for (let i = 0; i < (input**2); i++) {
+grid.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`;
+for (let i = 0; i < (sliderValue**2); i++) {
     grid.appendChild(document.createElement('div'));
 }
 
