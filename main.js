375e74ac135e6cd703939e776ea196506f1ec15d
Joel Josy <joeljosy79@gmail.com>
2023-09-08 23:17:07 +0530

    Connect slider to grid size to change no of tiles

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-let grid = document.querySelector('.grid');
+// Function to make the grid
+function createGrid(gridSize) {
 
-// Get no of tiles input
-let slider = document.getElementById('rangeSlider');
-let rangeOut = document.getElementById('rangeOutput');
-let sliderValue = slider.value;
+    // Clear existing tiles
+    if (grid.hasChildNodes()) {
+        let divs = document.querySelectorAll('.grid > div');
+        divs.forEach((div) => {
+            grid.removeChild(div)
+        });
 
+    }
 
-slider.oninput =  () => {
-    sliderValue = slider.value;
-    rangeOut.innerHTML = sliderValue;
-};
-
-
-// Create tiles
-grid.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`;
-for (let i = 0; i < (sliderValue**2); i++) {
-    grid.appendChild(document.createElement('div'));
+    // Create tiles
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    for (let i = 0; i < (gridSize**2); i++) {
+        grid.appendChild(document.createElement('div'));
+    }
+    
+    // Add class to each hovered tile
+    let divs = document.querySelectorAll('.grid > div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.classList.add('tileHover');
+        });
+    });
 }
 
+let grid = document.querySelector('.grid');
 
-// Add class to each hovered tile
-let divs = document.querySelectorAll('.grid > div')
-divs.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.classList.add('tileHover');
-    });
-});
+// Get no of tiles slider input
+let slider = document.getElementById('rangeSlider');
+let rangeOut = document.getElementById('rangeOutput');
+
+// Update slider output
+slider.oninput =  () => {
+    rangeOut.innerHTML = slider.value;
+};
+
+// Default grid
+createGrid(16)
+
+slider.addEventListener('mouseup', () => {createGrid(slider.value)})
+
+
