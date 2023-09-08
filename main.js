79c0ec0685f7960f84e1c2451d94252d9d70457b
Joel Josy <joeljosy79@gmail.com>
2023-09-08 23:29:50 +0530

    Add clear drawing button

--- main.js
+++ main.js
@@ -2,20 +2,14 @@
 function createGrid(gridSize) {
 
     // Clear existing tiles
-    if (grid.hasChildNodes()) {
-        let divs = document.querySelectorAll('.grid > div');
-        divs.forEach((div) => {
-            grid.removeChild(div)
-        });
-
-    }
+    clearGrid(1);
 
     // Create tiles
     grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     for (let i = 0; i < (gridSize**2); i++) {
         grid.appendChild(document.createElement('div'));
-    }
+    };
     
     // Add class to each hovered tile
     let divs = document.querySelectorAll('.grid > div');
@@ -24,9 +18,27 @@ function createGrid(gridSize) {
             div.classList.add('tileHover');
         });
     });
-}
+};
+
+function clearGrid(opt) {
+    if (opt == 1) {
+        if (grid.hasChildNodes()) {
+            let divs = document.querySelectorAll('.grid > div');
+            divs.forEach((div) => {
+                grid.removeChild(div)
+            });
+    
+        };
+    } else if (opt == 2) {
+        let divs = document.querySelectorAll('.grid > div');
+        divs.forEach((div) => {
+            div.classList.remove('tileHover')
+        });
+    };
+};
 
 let grid = document.querySelector('.grid');
+let clearButton = document.querySelector('#clearButton');
 
 // Get no of tiles slider input
 let slider = document.getElementById('rangeSlider');
@@ -34,12 +46,15 @@ let rangeOut = document.getElementById('rangeOutput');
 
 // Update slider output
 slider.oninput =  () => {
-    rangeOut.innerHTML = slider.value;
+    rangeOut.innerHTML = `${slider.value} x ${slider.value}`;
 };
 
-// Default grid
-createGrid(16)
+// Clear Button 
+clearButton.onclick = () => {clearGrid(2)};
 
-slider.addEventListener('mouseup', () => {createGrid(slider.value)})
+// Default grid
+createGrid(16);
+
+slider.addEventListener('mouseup', () => {createGrid(slider.value)});
 
 
